aabe99fbafc177f5c8e00a3f1dcd802c0c438e55
steven <steven.ray.purcell@gmail.com>
2019-08-11 14:38:29 -0500

    Minor update, comments.

--- gradient_descent/gradient_descent.py
+++ gradient_descent/gradient_descent.py
@@ -2,11 +2,15 @@ import sympy as sp
 from sympy.utilities.lambdify import lambdify, implemented_function
 import numpy as np
 
+# Set up SymPy for symbolic differentiation
 x = sp.Symbol('x')
 y = (x+5)**2
 yprime = y.diff(x)
+
+# Convert the first derivative of the input function to a lambda
 yprime = lambdify(x, yprime)
 
+# Instantiate gradient descent inputs
 cur_x = 10
 rate = 0.001
 precision = 0.000000000000001
@@ -14,11 +18,10 @@ prev_step_size = 1
 iters = 0
 max_iters = 25000
 
-df = yprime
-
+# Loop until precision meets threshold
 while prev_step_size > precision and iters < max_iters:
     prev_x = cur_x
-    cur_x = cur_x - rate * df(prev_x)
+    cur_x = cur_x - rate * yprime(prev_x)
     prev_step_size = abs(cur_x - prev_x)
     iters += 1
 
