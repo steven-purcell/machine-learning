74172989eb5adcf0e330eb9882ebc0ae5536fe36
steven <steven.ray.purcell@gmail.com>
2019-08-11 14:45:08 -0500

    Updates.

--- gradient_descent/gradient_descent.py
+++ gradient_descent/gradient_descent.py
@@ -15,16 +15,16 @@ cur_x = 10
 rate = 0.001
 precision = 0.000000000000001
 prev_step_size = 1
-iters = 0
-max_iters = 25000
+n = 0 # Current iteration
+max_n = 25000 # max iterations
 
 # Loop until precision meets threshold
-while prev_step_size > precision and iters < max_iters:
+while prev_step_size > precision and n < max_n:
     prev_x = cur_x
     cur_x = cur_x - rate * yprime(prev_x)
     prev_step_size = abs(cur_x - prev_x)
-    iters += 1
+    n += 1
 
-    print("Iteration",iters,"\nX value is",cur_x)
+    print("Iteration:",n,"\nX:",cur_x)
     
 print("The local minimum occurs at", cur_x)
